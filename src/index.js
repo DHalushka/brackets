40b624516631d2a1d6079fe70f2577c717dcaad1
DHalushka <galushkodaria@gmail.com>
2024-10-25 17:52:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const stack = [];
+	const openBrackets = {};
+	const closeBrackets = {};
+	bracketsConfig.forEach(([open, close]) => {
+		openBrackets[open] = close;
+		closeBrackets[close] = open;
+	});
+	for (let char of str) {
+		if (openBrackets[char]) {
+			if (openBrackets[char] === char) {
+				if (stack[stack.length - 1] === char) {
+					stack.pop();
+				} else {
+					stack.push(char);
+				}
+			} else {
+				stack.push(char);
+			}
+		} else if (closeBrackets[char]) {
+			if (stack.pop() !== closeBrackets[char]) {
+				return false;
+			}
+		}
+	}
+	return stack.length === 0;
+};
